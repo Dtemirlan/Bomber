e41ff7a9cbffad117d02b3d6217eac9d01463607
Dzhaparov Temirlan <temirlandzaparov820@gmail.com>
2023-10-30 14:40:07 +0600

    first commit

---
+++ src/ Components/Game.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import GameBoard from './GameBoard';
+import Counter from './Counter.tsx';
+import ResetButton from './ResetButton';
+
+const createItems = (): { hasItem: boolean; clicked: boolean }[] => {
+    const gridSize = 6;
+    const totalCells = gridSize * gridSize;
+
+    const emptyCells: { hasItem: boolean; clicked: boolean }[] = Array(totalCells).fill({
+        hasItem: false,
+        clicked: false,
+    });
+
+    const targetIndex = Math.floor(Math.random() * totalCells);
+
+    emptyCells[targetIndex].hasItem = true;
+
+    return emptyCells;
+};
+
+const Game: React.FC = () => {
+    const [items, setItems] = useState(createItems());
+    const [attempts, setAttempts] = useState<number>(0);
+
+    const handleCellClick = (index: number): void => {
+        const updatedItems = [...items];
+        updatedItems[index].clicked = true;
+
+        if (updatedItems[index].hasItem) {
+            setAttempts(attempts + 1);
+        }
+
+        setItems(updatedItems);
+    };
+
+    const handleReset = (): void => {
+        const newItems = createItems();
+        setItems(newItems);
+        setAttempts(0);
+    };
+
+    return (
+        <div className="game-container">
+            <h2>Hidden Item Game</h2>
+            <GameBoard items={items} onCellClick={handleCellClick} />
+            <Counter attempts={attempts} />
+            <ResetButton onReset={handleReset} />
+        </div>
+    );
+};
+
+export default Game;
